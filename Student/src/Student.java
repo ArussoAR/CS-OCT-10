6a6615e541928b86824b0a5278e25dc80931ff70
alexandrarusso <alexandrarusso@alexandras-mbp>
2020-10-10 21:14:49 -0400

    complete version

--- Student/src/Student.java
+++ Student/src/Student.java
@@ -3,34 +3,34 @@ public class Student {
 	
 	// method one is for the Students name
 	public void setStudentName(String name) {    //parameter is String
-		System.out.println("Student name");
+		System.out.println("Student name is: " + name);
 	}
 	
 	//method two is for the Students major
 	public void setStudentMajor(String major, int ID) {   //parameter is String and int 
-		System.out.println("Student major");
+		System.out.println("Student major is: " + major + ID);
 	}
 	
 	// method three is for the Student GPA
 	public void setStudentGPA(double gpa) {     // parameter is double GPA
-		System.out.println("Student GPA");
+		System.out.println("Student GPA is: " + gpa);
 	}
 	
 	public static void main(String[] args) {
 		//call the method by using the object 
-		Student information = new Student();
+		Student myStudent = new Student();
 		
 		// this is the method call for the Students name
-		information.setStudentName("Alex");
+		myStudent.setStudentName("Alex");
 		var name = "Alex";
 		
 		// this is the method call for the Students major
-		information.setStudentMajor("Business", 10064);
+		myStudent.setStudentMajor("Business", 10064);
 		var major = "Business";
 		int ID = 10064;
 		
 		// this is the method call for the Student GPA
-		information.setStudentGPA(7.86);
+		myStudent.setStudentGPA(7.86);
 		double gpa = 7.86;
 		
 
